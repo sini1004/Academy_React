ff12b1c732632314e72333f8eda00fafad32eb44
shinhee <tlsgml656@gmail.com>
2022-11-02 16:19:37 +0900

    useReducer 사용하여 상태 업데이트 로직 분리해보기

--- working/src/Counter.jsx
+++ working/src/Counter.jsx
@@ -1,16 +1,35 @@
 //useReducer를 이용한 Counter
 
-import React, {useState} from 'react'
+import React, {useReducer} from 'react'
+
+//reducer함수 만들기 : 상태를 업데이트 하는 함수 (컴퍼넌트 밖에 있음)
+//상태를 변경하는 로직이 있는 함수
+//state : 현재 상태(숫자, 문자배열, 객체,...), action : 함수 객체를 파라미터로 받아옴
+//컴포넌트 밖에서 작성, 다른 파일로 불러오는 것도 가능
+
+function reducer(state, action) {
+  //switch문 사용됨
+  switch(action.type) {
+    case 'INCREMENT' : 
+      return state + 1;
+    case 'DECREMENT' : 
+      return state - 1;
+    default : 
+      return state;
+  }
+}
 
 export default function Counter() {
 
-  const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
+  //state : 현재 상태, dispatch : 액션을 발생시키는 함수
+  //첫번째 파라미터는 연결되는 함수명, 두번째 파라미터는 초기값
 
   const increase = () => {
-    setNumber(number + 1);
+    dispatch({type : 'INCREMENT'});
   }
   const decrease = () => {
-    setNumber(number - 1);
+    dispatch({type : 'DECREMENT'});
   }
 
   return (
