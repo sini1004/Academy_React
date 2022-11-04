b252c8b3a33a6f159fbf64a8a02bc32c50c4dc80
shinhee <tlsgml656@gmail.com>
2022-11-04 09:59:54 +0900

    Props Drilling

--- working/src/Context.jsx
+++ working/src/Context.jsx
@@ -1,2 +1,22 @@
 //Context API 를 사용한 전역 값 관리
 //Context : 리액트 컴포넌트간에 어떠한 값을 공유할수 있게 해주는 기능
+
+import React, {createContext, useContext} from 'react';
+
+function Child({text}) {
+  return <div>안녕하세요? {text}</div>
+}
+
+function Parent({text}) {
+  return <Child text = {text} />
+}
+
+function GrandParent({text}) {
+  return <Parent text = {text} />
+}
+
+function Context({text}) {
+  return <GrandParent text = 'GOOD~'/>
+}
+
+export default Context
