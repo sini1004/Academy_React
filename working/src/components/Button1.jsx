603f8e31a57cb7918fa22fb90a9aed54bc387fb4
shinhee <tlsgml656@gmail.com>
2022-11-07 12:40:11 +0900

    css module

---
+++ working/src/components/Button1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button1.css'
+
+const Button1 = () => {
+  return (
+    <button className='Button'>Button1</button>
+  )
+}
+
+export default Button1
---
+++ working/src/components/Button2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button2.css'
+
+const Button2 = () => {
+  return (
+    <button className='Button'>Button2</button>
+  )
+}
+
+export default Button2
