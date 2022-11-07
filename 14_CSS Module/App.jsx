ff172a75f1ca139019727d1e64e5855d695d2b1b
shinhee <tlsgml656@gmail.com>
2022-11-07 12:50:47 +0900

    CSS_Module 방법 연습

---
+++ 14_CSS Module/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Button1 from './components/Button1';
+import Button2 from './components/Button2';
+// import Button1 from './components/Button1';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button1 />
+        <Button2 />
+      </div>
+    </div>
+  )
+}
+
+export default App;
--- working/src/components/Button2.jsx
+++ working/src/components/Button2.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import './Button2.css'
+import aa from './Button2.module.css' //styles 대신 다른 단어로도 가능
+
 
 const Button2 = () => {
   return (
-    <button className='Button'>Button2</button>
+    <button className={aa.Button}>Button2</button>
   )
 }
 
