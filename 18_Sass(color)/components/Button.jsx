e1031db5d93ed58677d00dd9470aa1bd489cae7b
shinhee <tlsgml656@gmail.com>
2022-11-08 11:51:22 +0900

    Sass color 사용 연습, 폴더 분리

--- 18_Sass(color)/components/Button.jsx
+++ 18_Sass(color)/components/Button.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import "./Button.scss";
 import classNames from "classnames"; //설치한 classNames 모듈 불러옴
 
-const Button = ({ children, size, color }) => {
-	return (
-		<button className={classNames("Button", size, color)}>{children}</button>
-	);
+const Button = ({ children, size }) => {
+	return <button className={classNames("Button", size)}>{children}</button>;
 	// return <button className={["Button", size]}>{children}</button>;
 };
 
 Button.defaultProps = {
 	size: "medium",
-	color: "indigo",
 };
 
 export default Button;
--- working/src/App.jsx
+++ working/src/App.jsx
@@ -5,10 +5,39 @@ import Button from "./components/Button";
 function App() {
 	return (
 		<div className="App">
-			<Button size="large">파란버튼</Button>
-			<Button>파란버튼</Button>
-			<Button size="small">파란버튼</Button>
-			{/* children prop : 컴포넌트를 html처럼 사용 */}
+			<div className="Btnwrap">
+				<Button size="large" color="orange">
+					오렌지버튼
+				</Button>
+				<Button color="orange">오렌지버튼</Button>
+				<Button size="small" color="orange">
+					오렌지버튼
+				</Button>
+				{/* children prop : 컴포넌트를 html처럼 사용 */}
+			</div>
+			<div className="Btnwrap">
+				<Button size="large" color="pink">
+					핑크버튼
+				</Button>
+				<Button color="pink">핑크버튼</Button>
+				<Button size="small" color="pink">
+					핑크버튼
+				</Button>
+			</div>
+			<div className="Btnwrap">
+				<Button size="large" color="teal">
+					틸버튼
+				</Button>
+				<Button color="teal">틸버튼</Button>
+				<Button size="small" color="teal">
+					틸버튼
+				</Button>
+			</div>
+			<div className="Btnwrap">
+				<Button size="large">기본버튼</Button>
+				<Button>기본버튼</Button>
+				<Button size="small">기본버튼</Button>
+			</div>
 		</div>
 	);
 }
