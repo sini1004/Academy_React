2f5ca716fe8b0c8b205525f874339361c7c81fdd
shinhee <tlsgml656@gmail.com>
2022-11-08 11:17:44 +0900

    Sass size지정, classnames 라이브러리 사용

--- working/src/App.jsx
+++ working/src/App.jsx
@@ -5,7 +5,9 @@ import Button from "./components/Button";
 function App() {
 	return (
 		<div className="App">
+			<Button size="large">파란버튼</Button>
 			<Button>파란버튼</Button>
+			<Button size="small">파란버튼</Button>
 			{/* children prop : 컴포넌트를 html처럼 사용 */}
 		</div>
 	);
