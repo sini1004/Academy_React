7214b36657ed275f6aea1f73a6fcf4b81af5afec
shinhee <tlsgml656@gmail.com>
2022-11-09 09:48:29 +0900

    Todo Project start

--- working/src/App.jsx
+++ working/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import TailwindComponent from "./TailwindComponent";
 
 function App() {
 	return (
 		<>
-			<TailwindComponent />
+			<h1>Todo</h1>
 		</>
 	);
 }
