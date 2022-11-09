30e678625eb3d1f4e2c429b9d18fe1e54abfb78d
shinhee <tlsgml656@gmail.com>
2022-11-09 09:46:22 +0900

    폴더 분리 및 저장

---
+++ 17_tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import TailwindComponent from "./TailwindComponent";
+
+function App() {
+	return (
+		<>
+			<TailwindComponent />
+		</>
+	);
+}
+
+export default App;
+
+/**
+ * tailwindcss : https://tailwindcss.com/
+ * yarn add -D tailwindcss
+ * yarn tailwindcss init
+ */
