716d685c39db85cbf34138af651e6e3c97947cd4
shinhee <tlsgml656@gmail.com>
2022-11-09 11:46:47 +0900

    key값 부여(uuid)

--- working/src/components/TodoAdd/TodoAdd.jsx
+++ working/src/components/TodoAdd/TodoAdd.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoAdd = ({ onAdd }) => {
 	const [text, setText] = useState("");
@@ -11,7 +12,7 @@ const TodoAdd = ({ onAdd }) => {
 		if (text.trim().length === 0) {
 			return;
 		}
-		onAdd({ id: "고유값", text, status: "active" });
+		onAdd({ id: uuidv4(), text, status: "active" });
 		setText("");
 	};
 
