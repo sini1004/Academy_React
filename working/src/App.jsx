89d4ca8232f8b91e57fd0a62ef38e547d2e04c93
shinhee <tlsgml656@gmail.com>
2022-11-14 12:49:49 +0900

    todo darkmode 버튼 작업

--- working/src/App.jsx
+++ working/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
+import { DarkModeProvider } from "./components/context/DarkModeContext";
 
 //어떤 필터가 있는지 컴포넌트 밖에서 정의
 const filters = ["all", "active", "completed"];
@@ -10,10 +11,10 @@ const App = () => {
 	const [filter, setFilter] = useState(filters[0]); // 초기값으로는 필터 중 첫 번째인 all로 시작
 
 	return (
-		<>
+		<DarkModeProvider>
 			<Header filters={filters} filter={filter} onFilterChange={setFilter} />
 			<TodoList filter={filter} />
-		</>
+		</DarkModeProvider>
 	);
 };
 
--- working/src/components/Header/Header.jsx
+++ working/src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
-import React from "react";
 import styles from "./Header.module.css";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const Header = ({ filters, filter, onFilterChange }) => {
+	const { darkMode, toggleDarkMode } = useDarkMode();
 	return (
 		<header className={styles.header}>
+			<button className={styles.toggle} onClick={toggleDarkMode}>
+				{!darkMode && <BsMoonFill />}
+				{darkMode && <BsSunFill />}
+			</button>
 			<ul className={styles.filters}>
 				{filters.map((value, index) => (
 					<li key={index}>
