35d4f940ec49fa37d1a8159f231d12dbdb63e385
shinhee <tlsgml656@gmail.com>
2022-11-18 11:13:05 +0900

    날씨 앱 만들기 시작

---
+++ weather/src/App.jsx
@@ -0,0 +1,15 @@
+/**
+ * 현재 내 위치값 가져오기
+ */
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>날씨 앱 만드는 중입니다.</h1> 
+    </>
+  );
+}
+
+export default App;
--- working/src/components/Users.jsx
+++ working/src/components/Users.jsx
@@ -1,43 +1,62 @@
 /**
- * 버튼을 눌러서 API를 재요청하는 기능을 구현
- * fetchUsers 함수를 바깥으로 꺼내주고, 버튼을 만들어서 해당 함수를 연결
- * -> 필요할 때 마다 불러올꺼니까!
+ * useReducer로 요청 상태 관리
+ * useReducer 를 사용하여 LOADING, SUCCESS, ERROR 액션에 따라 다르게 처리
  */
-import React, { useState, useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import axios from 'axios'
 
+function reducer(state, action){
+  switch (action.type){
+    case 'LOADING' : 
+    return {
+      loading: true,
+      data: null,
+      error: null
+    };
+    case 'SUCCESS' : 
+      return {
+        loading: false,
+        data: action.data,
+        error: null
+      };
+    case 'ERROR' : 
+      return {
+        loading: false,
+        data: null,
+        error: action.error
+      };
+
+    default :
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  //유저스 API가 있는지 없는지
-  const [users, setUsers] = useState(null);
-  //현재 로딩중인지 아닌지
-  const [loading, setLoading] = useState(false);
-  //에러가 났는지 안났는지
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer,{
+    loading: false,
+    data: null,
+    error: null
+  });
 
   const fetchUsers = async() => {
+    dispatch({type:'LOADING'});
     try {
-      // 요청 시작 할 때 error 와 users를 초기화 해줌
-      setUsers(null);
-      setError(null);
-      //로딩 시작 loading -> true
-      setLoading(true); 
-
       const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-      setUsers(response.data); //가져온 데이터를 users로 설정
+      dispatch({type:'SUCCESS', data: response.data}); //요청 성공
     } catch (e) {
-      setError(e);
+      dispatch({type:'ERROR', error:e});
     }
-    setLoading(false); //로딩이 끝났으니 false로 
   };
 
   useEffect(() => {
     fetchUsers();
-  },[]) //처음에만 작동
+  },[]) 
   
-  // 3가지 상태에 따라 다른 결과물 렌더링
+  const {loading, error, data:users} = state; //비구조할당 : data를 users로 바꿔줌
+  
   if(loading) return <div>로딩중...</div>;
   if(error) return <div>에러가 발생했습니다.</div>;
-  if(!users) return null; //users 값이 아직 없을 때에는 null 을 보여주도록 처리
+  if(!users) return null; 
 
   return (
     <div>
