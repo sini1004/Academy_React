fa9a720deae789b685a70b12e598f08853bcd245
shinhee <tlsgml656@gmail.com>
2022-11-22 10:27:40 +0900

    텍스트 수정

--- weather/src/compotents/WeatherBox.jsx
+++ weather/src/compotents/WeatherBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WeatherBox = ({weather}) => {
   return (
     <div className='weather_box'>
-      <div className='h3'>{weather?.name} </div>
+      <div className='h6'>{weather?.name} </div>
       <h2 className='h4'>온도 : {weather?.main.temp}°C <br/> 습도 : {weather?.main.humidity}%</h2>
       <h3 className='h4'>{weather && weather.weather[0].description}</h3>
     </div>
