14f1865da7d374fb3a7f9190fac89fec06d11f3a
shinhee <tlsgml656@gmail.com>
2022-11-29 12:39:44 +0900

    useState 사용

--- react_shopping/src/page/ProductAll.jsx
+++ react_shopping/src/page/ProductAll.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const ProductAll = () => {
+  const [productsList, setProductsList] = useState([]);
+
   const getProducts = async() => {
     let url = 'http://localhost:5000/products';
     let response = await fetch(url); //브라우저는 네트워크에 요청을 보내고 프로미스객체가 반환
     let data = await response.json();
 
-    console.log(data);
+    setProductsList(data);
   };
 
   useEffect(() => {
@@ -15,7 +17,10 @@ const ProductAll = () => {
   }, []);
 
   return (
-    <div>ProductAll</div>
+    <div>
+      ProductAll
+      {console.log('productsList 확인:', productsList)}
+    </div>
   )
 }
 
