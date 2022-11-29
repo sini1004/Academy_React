8d0841b800c588b89cfe01e69be1965946ea49fe
shinhee <tlsgml656@gmail.com>
2022-11-29 11:01:25 +0900

    class 네임 지정

--- react_shopping/src/components/Navbar.jsx
+++ react_shopping/src/components/Navbar.jsx
@@ -10,22 +10,20 @@ const Navbar = () => {
   const menuList = ['woman','divided','man','kids','childern','sports','sale'];
   return (
     <>
-      <div>
-        <div>
-          <FiLogIn /> <span>로그인</span>
-        </div>
+      <div className='login_btn'>
+        <FiLogIn /> <span>로그인</span>
       </div>
       <h1>
         <img src="img/logo.png" alt="H&M" />
       </h1>
       <nav>
-        <ul>
+        <ul className='menu_list'>
           {menuList.map((item) => (
             <li>{item}</li>
           ))}
         </ul>
       </nav>
-      <div>
+      <div className='search'>
         <BiSearch />
         <input type="text" placeholder='제품검색'/>
       </div>
