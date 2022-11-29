bd6a72eca44a4a3b1217ef0350de94da67528ca1
shinhee <tlsgml656@gmail.com>
2022-11-29 12:07:45 +0900

    json 파일 변경

--- react_shopping/src/components/Navbar.jsx
+++ react_shopping/src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 //$npm install react-icons --save : react-icon 설치
 //$yarn add sass : sass 설치
+// 참고 사이트 : https://www.npmjs.com/package/json-server#getting-started
+//db.json 파일을 프로젝트 폴더에 넣어준다.
+//npm install -g json-server : json server 설치
+//$ json-server --watch db.json --port 5000 : 작동
 
 import React from 'react'
 import { FiLogIn } from "react-icons/fi";
