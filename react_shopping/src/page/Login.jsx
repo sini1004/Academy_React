f1ef40eb92d3270c3d90684dfc3ab58ed6c3e346
shinhee <tlsgml656@gmail.com>
2022-12-05 12:06:07 +0900

    로그인페이지 레이아웃 작업

--- react_shopping/src/page/Login.jsx
+++ react_shopping/src/page/Login.jsx
@@ -5,7 +5,7 @@ import './Login.scss'
 const Login = () => {
   return (
     <div className='container login_area'>
-      <Form>
+      <Form className='login_form'>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
