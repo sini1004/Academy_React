38974315fcbae491b9e834396cf0b90c77d902b2
shinhee <tlsgml656@gmail.com>
2022-12-07 12:59:48 +0900

    index 소스 정리, 부트스트랩 연결

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,9 +1,16 @@
 import styles from './App.module.css';
+import { Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>연락처</h1>
+      <h1> 📞Phone Book</h1>
+      <Container>
+        <Row>
+          <Col>1 of 1</Col>
+          <Col>2 of 1</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
