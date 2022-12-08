37990f8f3a2beadb3d8a49b1e82ca34e19d2ee1c
shinhee <tlsgml656@gmail.com>
2022-12-08 12:10:45 +0900

    연락처 추가 작업

--- phonebook/src/redux/reducer/reducer.js
+++ phonebook/src/redux/reducer/reducer.js
@@ -1,8 +1,18 @@
 
-let initialState = {};
+let initialState = {contactList:[]}; // 초기값으로 contactList이름의 빈 배열
 
 function reducer (state=initialState, action){
-
+  switch (action.type) {
+    case 'ADD_CONTACT':
+      return {
+        ...state, contactList : [
+          ...state.contactList, 
+          {name:action.payload.name, phoneNumber:action.payload.phoneNumber}
+        ]
+      };
+    default: 
+      return {...state}
+  }
 }
 
 export default reducer
