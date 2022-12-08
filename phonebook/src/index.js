9bc90727ca393d728a103585778f3e743e2e9f99
shinhee <tlsgml656@gmail.com>
2022-12-08 10:57:14 +0900

    react-redux, redux 설치

--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -1,12 +1,13 @@
+// store를 제공해주기 위해 provider로 감싸줌.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
