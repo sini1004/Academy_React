e9329a64f92dfd9e09aa7577a2cddb0e43e2ec91
shinhee <tlsgml656@gmail.com>
2022-12-08 12:32:24 +0900

    연락처 추가 작업

--- phonebook/src/components/ContactList.jsx
+++ phonebook/src/components/ContactList.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import ContactItem from './ContactItem'
 import SearchBox from './SearchBox'
 
 const ContactList = () => {
+  const book = useSelector((state) => state.contactList);
+  console.log(book)
   return (
     <div>
       <SearchBox />
+      <ContactItem />
       <ContactItem />
     </div>
   )
