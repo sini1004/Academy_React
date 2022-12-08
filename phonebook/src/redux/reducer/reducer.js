0b42adf0595941480e60bb0f71f548728dbbbce0
shinhee <tlsgml656@gmail.com>
2022-12-08 12:13:19 +0900

    연락처 추가 작업-재분해 이용

--- phonebook/src/redux/reducer/reducer.js
+++ phonebook/src/redux/reducer/reducer.js
@@ -2,12 +2,16 @@
 let initialState = {contactList:[]}; // 초기값으로 contactList이름의 빈 배열
 
 function reducer (state=initialState, action){
-  switch (action.type) {
+  let {type, payload} = action; // action 재분해 이용
+  switch (type) {
     case 'ADD_CONTACT':
       return {
         ...state, contactList : [
           ...state.contactList, 
-          {name:action.payload.name, phoneNumber:action.payload.phoneNumber}
+          {
+            name:payload.name, 
+            phoneNumber:payload.phoneNumber
+          }
         ]
       };
     default: 
