2d6a1ed98bf3e22ba662db36bbb16e84d0749ea5
shinhee <tlsgml656@gmail.com>
2022-12-08 10:51:37 +0900

    search 스타일 정의

--- phonebook/src/components/SearchBox.jsx
+++ phonebook/src/components/SearchBox.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Form, Row, Col, Button } from 'react-bootstrap';
+import style from './SearchBox.module.css'
 
 const SerchBox = () => {
   return (
-    <form action="">
+    <form action="" className={style.form}>
       <Row>
         <Col xs md='9'>
           <Form.Control type="text" placeholder="이름을 입력해주세요." />
