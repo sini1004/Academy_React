127526ceb2f2bc9691b85b7787e28c133528ecef
shinhee <tlsgml656@gmail.com>
2022-12-12 13:23:19 +0900

    수정

--- ott/src/redux/store.js
+++ ott/src/redux/store.js
@@ -2,9 +2,9 @@ import { legacy_createStore, applyMiddleware } from 'redux';
 // createStore 가로줄 redux toolkit사용 유도
 import rootReducer from './reducers';
 import thunk from 'redux-thunk'
-import { composeWidthDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-let store = legacy_createStore(rootReducer, composeWidthDevTools (
+let store = legacy_createStore(rootReducer, composeWithDevTools (
   applyMiddleware(thunk)
 ));
 
