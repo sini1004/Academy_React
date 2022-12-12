4a3925206cf7efb24f0fb3033a46eb80d607ad33
shinhee <tlsgml656@gmail.com>
2022-12-12 13:09:31 +0900

    redux 적용

--- ott/src/index.js
+++ ott/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import App from './App';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import store from 'redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  // redux 적용 
+  <Provider store={store}> 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
+
+// 1. router 세팅
+// 2. redux 적용 <Provider>
