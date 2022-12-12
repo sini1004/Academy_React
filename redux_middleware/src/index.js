ed3f048dd3c0452bb7992ccaaf074653814afdef
shinhee <tlsgml656@gmail.com>
2022-12-12 11:22:07 +0900

    카운터 딜레이

--- redux_middleware/src/index.js
+++ redux_middleware/src/index.js
@@ -3,12 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
--- redux_middleware/src/modules/counter.js
+++ redux_middleware/src/modules/counter.js
@@ -1,4 +1,5 @@
-// reducer
+// 1. reducer
+// 2. thunk 미들웨어 적용 : 특정 thunk함수가 디스패치 되면 1초후 작동
 
 // action type
 const INCREASE = 'INCREASE';
@@ -8,15 +9,24 @@ const DECREASE = 'DECREASE';
 export const increase = () => ({ type : INCREASE });
 export const decrease = () => ({ type : DECREASE });
 
+// thunk
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(increase()), 1000);
+};
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(decrease()), 1000);
+};
+
+
 // 초기값 (객체가 아닌 숫자여도 됨.)
 const initialState = 0;
 
 export default function counter (state = initialState, action){
   switch(action.type){
     case INCREASE :
-      return state+1;
+      return state + 1;
     case DECREASE :
-      return state-1;
+      return state - 1;
     default :
       return state;
   }
