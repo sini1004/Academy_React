e2ac6c3230737ea4c3493a226f85f4cfc1c8dfff
shinhee <tlsgml656@gmail.com>
2022-12-12 13:20:24 +0900

    redux 세팅

--- ott/src/redux/store.js
+++ ott/src/redux/store.js
@@ -1,4 +1,11 @@
-import { createStore } from 'redux'
-import rootReducer from './reducers/index'
+import { legacy_createStore, applyMiddleware } from 'redux';
+// createStore 가로줄 redux toolkit사용 유도
+import rootReducer from './reducers';
+import thunk from 'redux-thunk'
+import { composeWidthDevTools } from 'redux-devtools-extension'
 
-let store = createStore(rootReducer);
+let store = legacy_createStore(rootReducer, composeWidthDevTools (
+  applyMiddleware(thunk)
+));
+
+export default store;
