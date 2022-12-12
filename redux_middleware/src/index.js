e00304b41fbfb457557f5d383e9fe7e63ee5ef60
shinhee <tlsgml656@gmail.com>
2022-12-12 12:18:25 +0900

    composeWithDevTools 사용해서 리덕스 개발자 도구 활성화

--- redux_middleware/src/index.js
+++ redux_middleware/src/index.js
@@ -8,8 +8,11 @@ import './index.css';
 import App from './App';
 import rootReducer from './modules';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
