89bd951da0e2253299b8b2a868c1c2b6817416f6
shinhee <tlsgml656@gmail.com>
2022-12-12 10:15:54 +0900

    redux 세팅

--- redux_middleware/src/index.js
+++ redux_middleware/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'react';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
--- redux_middleware/src/modules/counter.js
+++ redux_middleware/src/modules/counter.js
@@ -1,8 +1,23 @@
 // reducer
 
+// action type
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// 액션 생성 함수
+const increase = () => ({ type : INCREASE });
+const decrease = () => ({ type : DECREASE });
+
 // 초기값 (객체가 아닌 숫자여도 됨.)
 const initialState = 0;
 
 export default function counter (state = initialState, action){
-
+  switch(action.type){
+    case INCREASE :
+      return increase;
+    case DECREASE :
+      return decrease;
+    default :
+      return state
+  }
 }
