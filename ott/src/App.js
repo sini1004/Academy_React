9aa48c6c03e7b00fba43ac11a40ce320c416a052
shinhee <tlsgml656@gmail.com>
2022-12-12 12:48:39 +0900

    OTT 사이트-페이지 생성

--- ott/src/App.js
+++ ott/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>dda</h1>
+      <h1>OTT</h1>
     </div>
   );
 }
---
+++ ott/src/pages/MovieDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieDetail = () => {
+  return (
+    <div>MovieDetail-영화상세페이지입니다.</div>
+  )
+}
+
+export default MovieDetail
