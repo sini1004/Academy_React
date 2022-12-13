4aff9c69c3cd6ca4e3c20283ff6edbd2c0fa21e0
shinhee <tlsgml656@gmail.com>
2022-12-13 10:47:58 +0900

    useEffect 사용

--- ott/src/pages/Home.jsx
+++ ott/src/pages/Home.jsx
@@ -1,6 +1,12 @@
+// - api를 불러오기 위한 세팅 : useEffect 사용
+
 import React from 'react'
+import { useEffect } from 'react'
 
 const Home = () => {
+  // 화면이 켜지자마자 데이터 불러오기, Redux Middleware(비동기 방식)
+  useEffect(()=>{},[]);
+
   return (
     <div>Home-대문페이지입니다.</div>
   )
