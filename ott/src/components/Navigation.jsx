f3ad5981933b96e99054c33a330c9684ab6adbb4
shinhee <tlsgml656@gmail.com>
2022-12-13 09:58:39 +0900

    상단 메뉴 변경 및 추가, Link to 사용

--- ott/src/components/Navigation.jsx
+++ ott/src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
@@ -14,21 +15,10 @@ const Navigation = () => {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Link to='/'>Home</Link>
+            <Link to='#'>New</Link>
+            <Link to='/movies'>Movies</Link>
+            <Link to='#'>My List</Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -37,7 +27,7 @@ const Navigation = () => {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success" style={{background:'#fe1f5a'}}>Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
