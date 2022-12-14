fb07630e4a7c2c1b1bc2f97ab37ac33699477425
shinhee <tlsgml656@gmail.com>
2022-12-14 13:20:30 +0900

    스타일 작업

--- ott/src/components/Banner.jsx
+++ ott/src/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = ({movie}) => {
   console.log('?????: ',movie)
   return (
     <div className='banner'
-    style ={{backgroundImage:`url(https://image.tmdb.org/t/p/w1920_and_h600_multi_faces/${movie.backdrop_path})`}}>
+    style ={{backgroundImage:`url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`}}>
     <div className="banner_info">
       <h1>{movie.title}</h1>
       <p>{movie.overview}</p>
