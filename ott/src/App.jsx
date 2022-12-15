a83d845a833ad944973a2a488a70af5d843ec6ce
shinhee <tlsgml656@gmail.com>
2022-12-15 12:00:41 +0900

    수정

--- ott/src/App.jsx
+++ ott/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import MovieDetail from './pages/MovieDetail';
 import Movies from './pages/Movies';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/movies' element={<Movies />}></Route>
         <Route path='/movies/:id' element={<MovieDetail />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
--- ott/src/components/MovieCard.jsx
+++ ott/src/components/MovieCard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({item}) => {
   return (
     <div className='card'
     style={{
-      backgroundImage:`URL("https://www.themoviedb.org/t/p/w710_and_h400_multi_faces/yreuPT1RYFvWkQ3gjnjRP8vAq3o.jpg")`
-    }}>MovieCard
-    <h2>sss</h2>
+      backgroundImage:`URL("https://www.themoviedb.org/t/p/w710_and_h400_multi_faces/${item.backdrop_path}")`
+    }}>
+      <div className='card_info'>
+        <h2>{item.title}</h2>
+        <p>{item.vote_average}</p>
+      </div>
     </div>
   )
 }
